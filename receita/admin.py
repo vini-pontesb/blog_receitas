b988a1c1e014700dda6c4bda332fa787c9473e9c
vini-pontesb <viniciusbragacontatos@gmail.com>
2026-01-06 14:12:10 -0300

    Atualizações de models e admin

--- receita/admin.py
+++ receita/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Categoria, Receita
 
-# Register your models here.
+
+class Receitas(admin.ModelAdmin):
+    list_display = ['titulo', 'categoria']
+    search_fields = ['titulo']
+    list_filter = ['categoria']
+
+class Categorias(admin.ModelAdmin):
+    search_fields = ['nome']
+
+admin.site.register(Categoria, Categorias)
+admin.site.register(Receita, Receitas)
--- receita/models.py
+++ receita/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model): 
+    nome = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.nome
+
+class Receita(models.Model):
+    titulo = models.CharField(max_length=100)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
